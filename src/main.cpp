080afab9a0e52a04ca2011ff72c0d2de17c8163e
limxuanhui <limxuanhui_17@hotmail.com>
2024-12-24 01:29:35 +0800

    read file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello from Xenith lang" << std::endl;
+    std::string str = "Hello from Xenith lang";
+    std::cout << str << std::endl;
+
+    std::ofstream file("sample/main.xh");
+    file << str;
+    file.close();
 
     return 0;
 }
